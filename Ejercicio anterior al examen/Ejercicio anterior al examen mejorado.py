c07504ed39407655cb24c137a1754bfc6bc5b752
dverduzco6 <111399164+dverduzco6@users.noreply.github.com>
2022-09-18 17:18:39 -0500

    Create Ejercicio anterior al examen mejorado.py

---
+++ Ejercicio anterior al examen/Ejercicio anterior al examen mejorado.py
@@ -0,0 +1,11 @@
+def tablas(t:int, n:int):
+    for i in range(1,t+1):
+        tabla(i,n, 10)
+
+def tabla(t:int, n:int, spc:int):
+    for i in range(1,n+1):
+        print(f"{t:^{spc}} X {i:^{spc}} = {t*i:^{spc}}")
+
+t = int(input("Ingrese el rango de las tablas:"))
+n = int(input("Ingrese el rango de las tablas:"))
+tablas(t,n)
