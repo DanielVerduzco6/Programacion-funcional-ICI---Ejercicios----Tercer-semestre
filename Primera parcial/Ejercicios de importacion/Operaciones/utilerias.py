35e2be03f2a3361ceab673d47f031487423b7b7f
dverduzco6 <111399164+dverduzco6@users.noreply.github.com>
2022-09-18 17:32:15 -0500

    Create utilerias.py

---
+++ Primera parcial/Ejercicios de importacion/Operaciones/utilerias.py
@@ -0,0 +1,19 @@
+# def mensaje(msg:str):
+#     print(msg, "Desde la libreria")
+#
+#
+# if __name__ == "__main__":
+#     mensaje("Hola ICI")
+
+import suma as s
+import resta as r
+import division as d
+import multiplicacion as m
+import cuadrado as c
+
+if __name__ == "__main__":
+    print("suma:", s.suma(4,5))
+    print("resta:", r.resta(4, 5))
+    print("division:", d.division(4, 5))
+    print("multiplicacion:", m.multiplicacion(4, 5))
+    print("cuadrado:", c.cuadrado(4))
